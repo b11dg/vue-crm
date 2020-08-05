6236eef4035053ae645d20a8e408c8cf68d3421d
nickybondarinno <nickybondarinno@gmail.com>
2020-08-05 14:51:01 +0300

    create protected routes

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -28,6 +28,7 @@
 <script>
 import Navbar from '@/components/app/Navbar.vue';
 import Sidebar from '@/components/app/Sidebar.vue';
+import messages from '@/utils/messages';
 
 export default {
     name: 'main-layout',
@@ -45,6 +46,16 @@ export default {
         }
 
         this.loading = false;
+    },
+    computed: {
+        error() {
+            return this.$store.getters.error;
+        },
+    },
+    watch: {
+        error(fbError) {
+            this.$error(messages[fbError.code] || 'Что-то пошло не так');
+        },
     },
 };
 </script>
