a88c0f72ba8cc36059c130418b34afe374d48976
nickybondarinno <nickybondarinno@gmail.com>
2020-08-07 17:29:08 +0300

    add profile page logic, change name form

--- src/views/Profile.vue
+++ src/views/Profile.vue
@@ -4,14 +4,27 @@
             <h3>Профиль</h3>
         </div>
 
-        <form class="form">
+        <form class="form" @submit.prevent="submitHandler">
             <div class="input-field">
             <input
+                v-model="name"
                 id="description"
-                type="text">
+                type="text"
+                :class="{invalid: $v.name.$dirty && !$v.name.required}">
             <label for="description">Имя</label>
             <span
-                    class="helper-text invalid">name</span>
+                class="helper-text invalid"
+                v-if="$v.name.$dirty && !$v.name.required">
+                Введите имя
+            </span>
+            </div>
+            <div class="switch">
+                <label>
+                English
+                <input type="checkbox">
+                <span class="lever"></span>
+                Русский
+                </label>
             </div>
 
             <button class="btn waves-effect waves-light" type="submit">
@@ -21,3 +34,50 @@
         </form>
     </div>
 </template>
+
+<script>
+import { mapGetters } from 'vuex';
+import { required } from 'vuelidate/lib/validators';
+
+export default {
+    name: 'profile',
+    data: () => ({
+        name: '',
+    }),
+    validations: {
+        name: { required },
+    },
+    mounted() {
+        this.name = this.info.name;
+        setTimeout(() => {
+            // eslint-disable-next-line no-undef
+            M.updateTextFields();
+        }, 0);
+    },
+    methods: {
+        async submitHandler() {
+            if (this.$v.$invalid) {
+                this.$v.$touch();
+                return;
+            }
+
+            try {
+                await this.$store.dispatch('updateInfo', {
+                    name: this.name,
+                });
+            } catch (error) {
+                console.log(error);
+            }
+        },
+    },
+    computed: {
+        ...mapGetters(['info']),
+    },
+};
+</script>
+
+<style scoped>
+    .switch {
+        margin-bottom: 2rem;
+    }
+</style>
